454448cae83cbfc4f64291e8ced763abf5fce791
rish1003 <rishikamukherjee2004@gmail.com>
2025-11-09 23:48:28 +0530

    changes

--- brickbasket/views.py
+++ brickbasket/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def hello(request):
     context = {'name': 'Rishika'}
-    return render(request, 'main/hello.html', context)
+    return render(request, 'welcome.html', context)
