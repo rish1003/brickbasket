830904084746092df3ed16e27fe779002c2c5a60
rish1003 <rishikamukherjee2004@gmail.com>
2025-11-12 13:48:18 +0530

    first html/css added

--- brickbasket/urls.py
+++ brickbasket/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', hello, name="hello")
+    path('', landing, name="hello")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- brickbasket/views.py
+++ brickbasket/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .decorators import *
 
-def hello(request):
-    context = {'name': 'Ravleen'}
-    return render(request, 'welcome.html', context)
+
+def landing(request):
+    #first page user sees + login and sign up
+    return render(request, 'landing_login.html')
