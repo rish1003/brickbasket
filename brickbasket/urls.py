5774626a67b7a099306cf75f13e5b3ab7b330370
rish1003 <rishikamukherjee2004@gmail.com>
2025-11-13 23:42:16 +0530

    admin done + cart & checkout

--- brickbasket/urls.py
+++ brickbasket/urls.py
@@ -22,12 +22,27 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', landing, name="hello"),
-     path('signup/', signup, name='signup'),
-    path('login/', user_login, name='userlogin'),
+    path('', landing, name="landing"),
+    path('signup/', signup, name='signup'),
+    path('signin/', signin, name='signin'),
     path('logout/', user_logout, name='logout'),
-
+    path('admin_dashboard/', admin_dashboard, name='admin_dashboard'),
+    path('admin_approve/<int:vendor_id>/', approve_vendor, name='approve_vendor'),
+    path('admin_reject/<int:vendor_id>/', reject_vendor, name='reject_vendor'),
+    path("admin_user/<int:user_id>/toggle/", toggle_user_status, name="toggle_user_status"),
     path('user/', user_main, name="user_main"),
-    path('user/product', product_view, name="produSct")
+    path('vendor/', vendor_main, name="vendor_main"),
+    path('user/product', product_view, name="produSct"),
+    path("cart/data/",get_cart, name="cart_data"),
+    path("cart/add/", add_to_cart, name="add_to_cart"),
+    path("cart/<int:item_id>/inc/", increment_cart_item, name="cart_inc"),
+    path("cart/<int:item_id>/dec/", decrement_cart_item, name="cart_dec"),
+    path("cart/<int:item_id>/delete/", delete_cart_item, name="cart_delete"),
+    path("checkout/", checkout_page, name="checkout"),
+    path("order/process/", process_order, name="process_order"),
+    path("order/success/<int:order_id>/", order_success, name="order_success"),
+    
+
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
